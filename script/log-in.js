6db8aaaf31f8b74e89203b038c0c6b2c31068b3a
Nathalie Dorendorf <dorendorfnathalie@gmail.com>
2024-11-22 07:20:47 +0100

    added form log in

---
+++ script/log-in.js
@@ -0,0 +1,28 @@
+const urlParams = new URLSearchParams(window.location.search);
+const msg = urlParams.get('msg');
+
+if (msg) {
+    messageBox.innerHTML = msg;        
+} else {
+    // display: none;
+    messageBox.style.display = "none";
+}
+
+
+function logIn() {
+    let email = document.getElementById("email");
+    let password = document.getElementById("password");
+    let user = users.find(user => user.email === email.value && user.password === password.value);
+
+    if (user) {
+        console.log("User found");
+    //     localStorage.setItem("user", JSON.stringify(user));
+    //     window.location.href = "dashboard.html";
+    // } else {
+    //     messageBox.style.display = "block";
+    //     messageBox.innerHTML = "Invalid email or password";
+    // }
+    }
+}
+
+
--- script/register.js
+++ script/register.js
@@ -1,6 +1,10 @@
-let logInData = []; // in firebase auslagern und dort speichern
+let logInData = [{   
+        "email": "nathalie@test.de",
+        "password": "test123"
+    }
+]; // in firebase auslagern und dort speichern
 
-function logIn() {
+function addUser() {
     let email = document.getElementById("email");
     let password = document.getElementById("password");
 
@@ -13,4 +17,7 @@ function logIn() {
     console.log(logInData);
     email.value = "";
     password.value = "";
+
+    // Weiterleitung zun Login-Seite + Nachricht anzeigen: "Erfolgreiche Registrierung!"
+    window.location.href = "log-in.html?msg=You Signed up successfully";
 }
