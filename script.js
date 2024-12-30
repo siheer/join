a36fb3a6b0f273311d256b142c0be9c5ab0f1e87
Nathalie Dorendorf <dorendorfnathalie@gmail.com>
2024-12-30 21:15:41 +0100

    added filter invert on sidebar icons

--- script.js
+++ script.js
@@ -48,10 +48,3 @@ function toggleUserMenu() {
     userMenu.classList.toggle('hidden');
 }
 
-// function toggleUserMenu() {
-//     let userMenu = document.getElementById('userMenu');
-//     if (userMenu.style.display === 'none') {
-//         userMenu.style.display = 'block';
-//     } else {
-//         userMenu.style.display = 'none';
-//     }
