bda08992aa62bdc013f09156fc9d388ef74f7a25
MarcoHantel <hantelmarco@gmailcom>
2025-01-06 13:57:50 +0100

    disable authentication.js, so that you can login without account

--- script/before-unload.js
+++ script/before-unload.js
@@ -1,3 +1,22 @@
+// window.addEventListener("beforeunload", () => {
+//     localStorage.removeItem("isLoggedIn");
+// });
+
+// Flag setzen, wenn die Seite geladen wird
+if (!sessionStorage.getItem("pageReloaded")) {
+    sessionStorage.setItem("pageReloaded", "true");
+}
+
+// EventListener für beforeunload
 window.addEventListener("beforeunload", () => {
-    sessionStorage.removeItem("isLoggedIn");
+    // Überprüfen, ob die Seite nicht neu geladen wurde
+    if (sessionStorage.getItem("pageReloaded")) {
+        sessionStorage.removeItem("isLoggedIn");
+        sessionStorage.removeItem("pageReloaded"); // Flag zurücksetzen
+    }
+});
+
+// Dies wird beim Neuladen der Seite gelöscht, aber nicht beim Schließen
+window.addEventListener("unload", () => {
+    sessionStorage.removeItem("pageReloaded"); //beim Neuladen zurücksetzen
 });
