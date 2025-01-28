db1f1989698fcc261cb3e39c011f54c270363a96
Felix <geissq@gmail.com>
2025-01-28 15:04:18 +0100

    Added comments and shortened several functions for improved clarity and maintainability

--- script/add-task/add-task-temlates.js
+++ script/add-task/add-task-temlates.js
@@ -1,36 +1,51 @@
+/**
+ * Generates the HTML template for rendering a contact.
+ * 
+ * @param {Array} renderArry - Array of contact objects.
+ * @param {number} index - Index of the contact to render in the array.
+ * @returns {string} - HTML string for the contact.
+ */
 function contactsTemlates(renderArry, index) {
   return ` 
-    <div>
-      <label class="checkbox-content">
-        <div class="custom-checkbox">
-          <img
-            id="${renderArry[index].id}"
-            src="/assets/icons/check-button-unchecked.svg"
-            alt="Unchecked"
-            class="unchecked"
-          />
-        </div>
-        <input
-          type="checkbox"
-          value="${renderArry[index].id}"
-          name="assignedTo"
-          id="${renderArry[index].id}"
-          onchange="checkt(event, '${renderArry[index].initials}', '${renderArry[index].color}','${renderArry[index].id}')"
-        />
-        <div class="name-badge">
-          ${renderArry[index].name}
-          <div
-            style="background-color: var(${renderArry[index].color})"
-            class="badge"
-          >
-            ${renderArry[index].initials}
-          </div>
-        </div>
-      </label>
+    
+<div>
+  <label class="checkbox-content">
+    <div class="custom-checkbox">
+      <img
+        id="${renderArry[index].id}"
+        src="/assets/icons/check-button-unchecked.svg"
+        alt="Unchecked"
+        class="unchecked"
+      />
     </div>
-  `;
+    <input
+      type="checkbox"
+      value="${renderArry[index].id}"
+      name="assignedTo"
+      id="${renderArry[index].id}"
+      onchange="checkt(event, '${renderArry[index].initials}', '${renderArry[index].color}','${renderArry[index].id}')"
+    />
+    <div class="name-badge">
+      ${renderArry[index].name}
+      <div
+        style="background-color: var(${renderArry[index].color})"
+        class="badge"
+      >
+        ${renderArry[index].initials}
+      </div>
+    </div>
+  </label>
+</div>
+
+`;
 }
 
+/**
+ * Generates the HTML template for rendering a list item (subtask).
+ * 
+ * @param {number} indexSubtask - Index of the subtask in the `subtask` array.
+ * @returns {string} - HTML string for the subtask.
+ */
 function renderList(indexSubtask) {
   return `
     <li ondblclick="edit('${indexSubtask}');">
@@ -54,13 +69,26 @@ function renderList(indexSubtask) {
   `;
 }
 
+/**
+ * Generates the HTML template for rendering a badge.
+ * 
+ * @param {string} color - CSS variable representing the color of the badge.
+ * @param {string} initials - Initials to display inside the badge.
+ * @returns {string} - HTML string for the badge.
+ */
 function renderBadge(color, initials) {
   return `
   <div style="background-color: var(${color});" class="badge margin0">${initials}</div>
   `;
 }
 
+/**
+ * Generates the HTML template for rendering a message when no contacts are found.
+ * 
+ * @returns {string} - HTML string indicating no contacts were found.
+ */
 function renderEmptyContacts() {
   return `<div class="checkbox-content no-results"> no contact found</div>
   `;
 }
+
